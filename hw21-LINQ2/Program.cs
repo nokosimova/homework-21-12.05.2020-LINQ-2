ac8dc631038093ee1068056779516f52d003a7be
kosimovanodira <nkosimova@gmail.com>
2020-05-15 19:57:32 +0500

    Добавьте файлы проекта.

---
+++ hw21-LINQ2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+namespace hw21_LINQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write some text:  ");
+            char[] input = Console.ReadLine().ToCharArray();
+            Console.Write("Result text:  ");
+            var answer = new string(input.Select(i => i == 'a' ? '1': i == 'e'? '2' : 
+                                                      i == 'i' ? '3': i == 'o'? '4':
+                                                      i == 'u' ? '5': i).ToArray());
+            Console.Write(answer);
+            Console.ReadKey();
+
+        }
+    }
+    
+}
