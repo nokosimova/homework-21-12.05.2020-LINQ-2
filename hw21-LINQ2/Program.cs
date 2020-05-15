1102d245ac7ecab2e20433b2104efb01b2819297
kosimovanodira <nkosimova@gmail.com>
2020-05-15 20:21:09 +0500

    solve problem 2

--- hw21-LINQ2/Program.cs
+++ hw21-LINQ2/Program.cs
@@ -14,8 +14,26 @@ namespace hw21_LINQ2
                                                       i == 'i' ? '3': i == 'o'? '4':
                                                       i == 'u' ? '5': i).ToArray());
             Console.Write(answer);
+       
+            Console.WriteLine("---Problem-2---");
+            Console.Write("Write some text:  ");
+            input = Console.ReadLine().ToCharArray();
+            Console.Write("Result text:  ");
+            answer = new string(input.Select(i => i == '1' ? 'a' : i == '2' ? 'e' :
+                                                  i == '3' ? 'i' : i == '4' ? 'o' :
+                                                  i == '5' ? 'u' : i).ToArray());
+            Console.Write(answer);
+        
+            Console.WriteLine("---Problem-3---");
+            Console.Write("Write some text:  ");
+            input = Console.ReadLine().ToCharArray();
+            var num1 = new string(input.TakeWhile(i => i != '+' && i != '-' && i != '*' && i != '/').
+                                      Where(i => (int)i >= 48 && (int)i <= 57).ToArray());
+            var oper = input.First(i => i == '+' || i == '-' || i == '*' || i == '/');
+            var num2 = new string(input.SkipWhile(i => i != '+' && i != '-' && i != '*' && i != '/').
+                                      Where(i => (int)i >= 48 && (int)i <= 57).ToArray());
+            Console.Write($"{num1} {oper} {num2}");
             Console.ReadKey();
-
         }
     }
     
