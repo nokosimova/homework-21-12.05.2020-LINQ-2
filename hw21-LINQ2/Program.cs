69981e1e48735c4512443490902e41aa585412f6
kosimovanodira <nkosimova@gmail.com>
2020-05-15 23:14:51 +0500

    try to finish problem 4

--- hw21-LINQ2/Program.cs
+++ hw21-LINQ2/Program.cs
@@ -6,6 +6,7 @@ namespace hw21_LINQ2
     {
         static void Main(string[] args)
         {
+            //-----//
             Console.WriteLine("---Problem-1---");
             Console.Write("Write some text:  ");
             char[] input = Console.ReadLine().ToCharArray();
@@ -14,7 +15,7 @@ namespace hw21_LINQ2
                                                       i == 'i' ? '3': i == 'o'? '4':
                                                       i == 'u' ? '5': i).ToArray());
             Console.Write(answer);
-       
+            //-----//
             Console.WriteLine("---Problem-2---");
             Console.Write("Write some text:  ");
             input = Console.ReadLine().ToCharArray();
@@ -23,33 +24,44 @@ namespace hw21_LINQ2
                                                   i == '3' ? 'i' : i == '4' ? 'o' :
                                                   i == '5' ? 'u' : i).ToArray());
             Console.Write(answer);
-        
+            //------//
             Console.WriteLine("---Problem-3---");
             Console.Write("Write some text:  ");
             input = Console.ReadLine().ToCharArray();
             var num1 = new string(input.TakeWhile(i => i != '+' && i != '-' && i != '*' && i != '/').
-                                      Where(i => (int)i >= 48 && (int)i <= 57).ToArray());
+                                      Where(i => i >= '0' && i <= '9').ToArray());
             var num2 = new string(input.SkipWhile(i => i != '+' && i != '-' && i != '*' && i != '/').
-                                      Where(i => (int)i >= 48 && (int)i <= 57).ToArray());
+                                      Where(i => i >= '0' && i <= '9').ToArray());
             var oper = input.First(i => i == '+' || i == '-' || i == '*' || i == '/');
+            answer = Calculate(num1, oper, num2);
+            Console.WriteLine($"Result : {answer}");
+            Console.WriteLine($"Because {int.Parse(num1)} {oper} {int.Parse(num2)} = {answer}");
+            //------//
+            Console.WriteLine("---Problem-4---");
+            Console.Write("Write some camelText:  ");
+            input = Console.ReadLine().ToCharArray();
+            char[] space = new string(' ', input.Length).ToCharArray();
+            var ans = input.Zip(space, (ltr, spc) => (ltr >= 'A' && ltr <= 'Z') ? spc + ltr : ltr);
+            foreach (var i in ans)
+                Console.Write(i);
             Console.ReadKey();
         }
 
-        public string Calculate(string num1, string oper, string num2)
+        public static string Calculate(string num1, char oper, string num2)
         {
             decimal ans = 0;
             switch (oper)
             {
-                case "+":
+                case '+':
                     ans = int.Parse(num1) + int.Parse(num2);
                     break;
-                case "-":
+                case '-':
                     ans = int.Parse(num1) - int.Parse(num2);
                     break;
-                case "*":
+                case '*':
                     ans = int.Parse(num1) * int.Parse(num2);
                     break;
-                case "/":
+                case '/':
                     ans = int.Parse(num1) / int.Parse(num2);
                     break;
                 default:
